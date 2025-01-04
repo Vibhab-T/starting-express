6290fec9e88971fd6eb6a1cb1b33f65c58d8ad66
Vibhab-T <vibhabtimsina86@gmail.com>
2025-01-04 21:17:21 +0545

    used routing. used body-parser to parse the incoming url encoded data in request

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log("In the first middleware!");
-  next();
+app.use(bodyParser.urlencoded());
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<html><body><form action="/product" method="POST"><input type="text" name="productTitle"><button type="submit">Add Product</button></form></body></html>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In the second middleware!");
-  res.send("<h1>Hello From Express</h1>");
+//app.post is just app.use for post requests. similarly, app.get
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/  ");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<html><body><h1>Hello From Express</h1></body></html>");
 });
