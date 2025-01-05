b165cfa6c40c1de2e73f243447d0501f73a3cac7
Vibhab-T <vibhabtimsina86@gmail.com>
2025-01-05 17:29:34 +0545

    dirname utility

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const path = require("path");
+const dirName = require("../utility/path");
 
 const router = express.Router();
 
 router.use("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(dirName, "views", "add-product.html"));
 });
 
 router.use("/product-added", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "product-added.html"));
+  res.sendFile(path.join(dirName, "views", "product-added.html"));
 });
 
 //app.post or router.post is just app.use for post requests. similarly, app.get
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
+const dirName = require("../utility/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.sendFile(path.join(dirName, "views", "shop.html"));
 });
 
 module.exports = router;
